8f326ebcae61aad12eb66acf5e53800f81952b52
jdiez17 <jose.manuel.diez@gmail.com>
2014-12-22 19:00:34 +0000

    Fork on respondWithHaskell

--- IO/Haskell.hs
+++ IO/Haskell.hs
@@ -2,6 +2,8 @@ module IO.Haskell where
 
 import System.IO
 import System.Process
+import Control.Concurrent (forkIO)
+import Control.Monad.Reader (runReaderT, ask)
 
 import IRC.Parser (Message)
 import IRC.Commands (respondMany)
@@ -20,5 +22,10 @@ runHaskell cmd = do
     return $ lines contents ++ lines errors
 
 respondWithHaskell :: Message -> String -> Bot ()
-respondWithHaskell m cmd = (io $ runHaskell cmd) >>= \result -> respondMany m result
+respondWithHaskell m cmd = do
+    st <- ask
+    _ <- io $ forkIO $ runReaderT action st
 
+    return ()
+    where
+        action = (io $ runHaskell cmd) >>= \result -> respondMany m result
